8b2ab7a21a3ca156c8a75be382a81452035c6488
Andrew <andrew@coulter.info>
2026-07-16 10:26:25 -0600

    Added fix for vpn so that it will pass the creds from the browser to gp.

--- home/vpn.nix
+++ home/vpn.nix
@@ -1,13 +1,17 @@
 # home/vpn.nix
 { pkgs, ... }:
+let
+  gpCallbackScript = pkgs.writeShellScript "gpcallback" ''
+    echo -n "$1" | ${pkgs.netcat}/bin/nc -w1 127.0.0.1 "$(cat /tmp/gpcallback.port)"
+  '';
+in
 {
   home.packages = with pkgs; [ gpclient openconnect netcat ];
-
   xdg.mimeApps.enable = true;
   xdg.desktopEntries.globalprotectcallback = {
     name = "GlobalProtect Callback";
     type = "Application";
-    exec = "bash -c 'echo -n \"%u\" | nc -w1 127.0.0.1 $(cat /tmp/gpcallback.port)'";
+    exec = "${gpCallbackScript} %u";
     mimeType = [ "x-scheme-handler/globalprotectcallback" ];
     noDisplay = true;
   };
